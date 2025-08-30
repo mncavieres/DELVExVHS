0781f605e39f4a4aff7b3c69a6f25464880c177f
fawkez <mncavieres@uc.cl>
2025-08-30 12:59:25 +0200

    started analyzing xshooter data

--- scripts/compute_altaz.py
+++ scripts/compute_altaz.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
 Compute Alt-Az coordinates and airmass statistics for target stars during a given night.
+This version ensures the night is defined properly (sunset on the given date and sunrise on the next)
+and parallelizes the per-star computations across 6 threads.
 """
 
 import numpy as np
@@ -10,11 +12,12 @@ import astropy.units as u
 from astroplan import Observer
 from astropy.table import Table
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
 def compute_star_airmass(observatory_lat, observatory_lon, observatory_height, date, stars, n_steps=1000):
     """
     Computes the minimum and median airmass for a list of stars over the night.
-
+    
     Parameters:
     -----------
     observatory_lat : float
@@ -24,15 +27,15 @@ def compute_star_airmass(observatory_lat, observatory_lon, observatory_height, d
     observatory_height : float
         Height of the observatory in meters.
     date : str
-        Date of observation in 'YYYY-MM-DD' format.
+        Date of observation in 'YYYY-MM-DD' format. This date is used to get the sunset on that day.
     stars : list of dicts
         List of target stars. Each star should be a dict with keys:
           - 'name': Name of the star.
-          - 'ra': Right Ascension (e.g. '18h36m56.33635s').
-          - 'dec': Declination (e.g. '+38d47m01.2802s').
+          - 'ra': Right Ascension in degrees.
+          - 'dec': Declination in degrees.
     n_steps : int, optional
         Number of time steps to sample during the night (default is 1000).
-
+    
     Returns:
     --------
     results : list of dicts
@@ -48,88 +51,90 @@ def compute_star_airmass(observatory_lat, observatory_lon, observatory_height, d
                              lon=observatory_lon*u.deg,
                              height=observatory_height*u.m)
     observer = Observer(location=location, timezone='UTC')
-
-    # Define the time at midnight UTC on the given date
-    time_midnight = Time(date + " 00:00:00")
     
-    # Compute sunset and sunrise times for the night
-    sunset = observer.sun_set_time(time_midnight, which='next')
-    sunrise = observer.sun_rise_time(time_midnight, which='next')
-
+    # Use noon on the given date as reference to ensure the computed sunset is in the evening
+    time_noon = Time(date + " 12:00:00")
+    sunset = observer.sun_set_time(time_noon, which='next')
+    # Compute the sunrise following that sunset (thus on the next day)
+    sunrise = observer.sun_rise_time(sunset, which='next')
+    
     # Create a time grid spanning from sunset to sunrise
     night_times = sunset + (sunrise - sunset) * np.linspace(0, 1, n_steps)
+    # Precompute the AltAz frame for the whole night
+    altaz_frame = AltAz(obstime=night_times, location=location)
 
-    results = []
-    for star in tqdm(stars):
-        # Create a SkyCoord object for the target star (assumed to be in ICRS)
-        star_coord = SkyCoord(star['ra']*u.deg, star['dec']*u.deg, frame='icrs')
-        
-        # Define the AltAz frame for the entire night at the observatory location
-        altaz_frame = AltAz(obstime=night_times, location=location)
-        
-        # Transform the star's coordinates into the Alt-Az frame
-        star_altaz = star_coord.transform_to(altaz_frame)
-        
-        # Compute airmass using the sec(z) attribute (only valid above horizon)
-        airmass = star_altaz.secz
-        
-        # Consider only times when the star is above the horizon (altitude > 0)
-        above_horizon = star_altaz.alt > 0*u.deg
-        valid_airmass = airmass[above_horizon]
-        
-        if len(valid_airmass) > 0:
-            min_airmass = np.min(valid_airmass)
-            median_airmass = np.median(valid_airmass)
-        else:
+    def process_star(star):
+        """Process a single star and compute its airmass stats."""
+        try:
+            # Create a SkyCoord object for the target star (RA, Dec in degrees)
+            star_coord = SkyCoord(star['ra']*u.deg, star['dec']*u.deg, frame='icrs')
+            # Transform the star's coordinates into the AltAz frame
+            star_altaz = star_coord.transform_to(altaz_frame)
+            # Compute airmass using the sec(z) attribute (only valid above the horizon)
+            airmass = star_altaz.secz
+            # Consider only times when the star is above the horizon (altitude > 0)
+            above_horizon = star_altaz.alt > 0*u.deg
+            valid_airmass = airmass[above_horizon]
+            if len(valid_airmass) > 0:
+                min_airmass = np.min(valid_airmass)
+                median_airmass = np.median(valid_airmass)
+            else:
+                min_airmass = np.nan
+                median_airmass = np.nan
+        except Exception as e:
+            # In case of any error, return nan values.
             min_airmass = np.nan
             median_airmass = np.nan
-        
-        results.append({
+
+        return {
             'name': star['name'],
             'min_airmass': min_airmass,
             'median_airmass': median_airmass,
             'sunset': sunset.iso,
             'sunrise': sunrise.iso
-        })
+        }
+    
+    # Use ThreadPoolExecutor to process stars in parallel using 6 threads.
+    with ThreadPoolExecutor(max_workers=6) as executor:
+        results = list(tqdm(executor.map(process_star, stars), total=len(stars)))
     
     return results
 
 # Example usage
 if __name__ == "__main__":
-    # Define observatory parameters (LCO)
-    observatory_lat = -29.015972222222      # degrees
-    observatory_lon = -70.692083333333    # degrees
-    observatory_height = 2380      # meters
+    # Define observatory parameters (e.g., LCO)
+    observatory_lat = -29.015972222222  # degrees
+    observatory_lon = -70.692083333333   # degrees
+    observatory_height = 2380            # meters
 
-    # Set the observation date
+    # Set the observation date (this date corresponds to the evening's sunset)
     date = "2025-03-13"
 
+    # Load catalog from a FITS file (adjust the path as needed)
+    data = Table.read('/Users/mncavieres/Documents/2024-1/Delve/Data/for_mike.fits')
 
-    # load catalog 
-    data = Table.read('/Users/mncavieres/Documents/2024-1/Delve/filtered_stars.fits')
-
-    # make a dictionary with source_id and coordinates
+    # Build a list of stars with source_id and coordinates (RA, Dec in degrees)
     stars = []
     for i in range(len(data)):
-        stars.append({'name': data['source_id'][i], 'ra': data['ra'][i], 'dec': data['dec'][i]})
-
+        stars.append({
+            'name': data['source_id'][i],
+            'ra': data['ra'][i],
+            'dec': data['dec'][i]
+        })
 
     # Compute the airmass statistics for each target star
     results = compute_star_airmass(observatory_lat, observatory_lon, observatory_height, date, stars)
     
-    # save results to a csv using pandas
+    # Save results to a CSV using pandas
     import pandas as pd
     data_frame = pd.DataFrame(results)
-    data_frame.to_csv('/Users/mncavieres/Documents/2024-1/Delve/Proposal/mike/airmass_results.csv', index=False)
+    #data_frame.to_csv('/Users/mncavieres/Documents/2024-1/Delve/Proposal/mike/airmass_results.csv', index=False)
 
-    # merge results with the catalog
+    # Merge results with the catalog
     input_data_df = data.to_pandas()
     results_df = pd.DataFrame(results)
-
-    # merge the dataframes using the name column and source_id
     merged_df = pd.merge(input_data_df, results_df, left_on='source_id', right_on='name')
-    merged_df.to_csv('/Users/mncavieres/Documents/2024-1/Delve/Proposal/mike/filtered_stars_with_airmass.csv', index=False)
-
+    merged_df.to_csv('/Users/mncavieres/Documents/2024-1/Delve/Proposal/mike/filtered_stars_with_airmass2.csv', index=False)
 
     # Print the results
     for res in results:
---
+++ xshooter/run_rvspecfit.py
@@ -0,0 +1,118 @@
+from rvspecfit import fitter_ccf, vel_fit, spec_fit, utils
+from astropy.table import Table
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+config=utils.read_config('/Users/mncavieres/Documents/2024-1/Delve/xshooter/config.yaml') # optional
+# you can create a configuration file with various options
+
+
+run_path = '/Users/mncavieres/Documents/2024-1/Delve/xshooter/Spec/Star01'
+# let's assume we have data stored in 1d arrays in a table
+# with wavelength being in Angstrom
+# espec being a vector of standard deviations
+
+tab=Table().read('/Users/mncavieres/Documents/2024-1/Delve/xshooter/Spec/Star01/ADP.2025-07-13T14_06_10.099.fits', hdu=1) # read the fits file
+wavelength = tab['WAVE']
+spec = tab['FLUX_REDUCED'] #+ 2
+espec = tab['ERR_REDUCED'] # add a small value to avoid division by zero
+
+# reduce range
+lim_mask = (wavelength.value > 12000) & (wavelength.value < 24780)
+wavelength = wavelength[lim_mask]
+spec = spec[lim_mask]
+espec = espec[lim_mask]
+
+
+# make a badmask to exclude the weird line at 2272
+badmask = np.zeros_like(wavelength.value, dtype=bool)
+badmask[(wavelength.value > 2270) & (wavelength.value < 2275)] = True  # weird line
+#badmask = ~badmask
+
+#(wavelength < 4500) & (wavelength > 3600)#wavelength < 4500 # only fit the first chip
+# make a badmask that includes the regions between the chips and the weird thing on the third chip
+# badmask = np.zeros_like(wavelength.value, dtype=bool)
+# # mark the regions between the chips as bad
+# # remove the begining
+# badmask[wavelength.value < 3750] = True  # inital weird part
+# badmask[(wavelength.value > 4500) & (wavelength.value < 4650)] = True  # first gap
+# badmask[(wavelength.value > 5830) & (wavelength.value < 5960)] = True  # second gap
+# badmask[(wavelength.value > 6505) & (wavelength.value < 6515)] = True  # weird thing on the third chip
+# badmask[(wavelength.value > 6540) & (wavelength.value < 6620)] = True  # Halpha region which for some reason is being fit as continuum
+# badmask[(wavelength.value > 7200) & (wavelength.value < 7215)] = True  # weird thing on the third chip
+
+
+# # also mask the last few pixels of the spectrum
+# badmask[-10:] = True  # last 10 pixels
+
+# invert the badmask to get the good mask
+#badmask = ~badmask
+# # set espec to a huge value where the badmask is True
+espec[badmask] = 1e5
+
+
+
+
+
+
+# This constructs the specData object from wavelength, spectrum and error
+# spectrum arrays. The rvspecfit works on arrays of SpecData's which may
+# represent multiple exposures or multiple spectral configurations
+# Here we just have one spectrum from the spectral configuration "mysetup"
+
+specdata = [spec_fit.SpecData('xshooter_test',
+                               wavelength,
+                               spec,
+                               espec#,
+                               #badmask=badmask
+                               )
+                              ]
+
+
+# this tries to get a sensible guess for the stellar parameters/velocity
+res = fitter_ccf.fit(specdata, config)
+paramDict0 = res['best_par']
+print(paramDict0)
+# set the initial guess for the parameters using Kreuzer+2020
+#paramDict0['teff'] = 11400 #res['best_teff']
+#paramDict0['logg'] = 3.79
+#paramDict0['feh'] = 0 #res['best_feh']
+
+
+fixParam = [] 
+# if res['best_vsini'] is not None:
+#     paramDict0['vsini'] = 47#res['best_vsini']
+
+options = {'npoly':15}
+
+# this does the actual fitting performing the maximum likelihood fitting of the data
+res1 = vel_fit.process(specdata,
+                           paramDict0,
+                           #fixParam=fixParam,
+                           config=config,
+                           options=options)
+print(res1)
+
+# save the results to a pickle file
+import pickle
+
+with open("/Users/mncavieres/Documents/2024-1/Delve/xshooter/Spec/Star01/res_vacuum_default_full.pkl", "wb") as f:
+    pickle.dump(res1, f)
+
+
+# change the badmask on the Halpha region to true
+#badmask[(wavelength.value > 6540) & (wavelength.value < 6620)] = False
+
+# same plot but with matplotlib
+plt.figure(figsize=(30/1.5, 10/1.5))
+plt.plot(wavelength[~badmask], spec[~badmask], color='black', label='Spectrum')
+plt.plot(wavelength, res1['yfit'][0], color='red', label='Fit')
+plt.xlabel('Wavelength ')
+plt.ylabel('flux')
+plt.title('1D Coadded Spectrum with Fit')
+plt.legend()
+plt.grid()
+plt.savefig(os.path.join(run_path,'rvspecfit_results.pdf'), bbox_inches='tight')
+plt.show()
+#Table(res1).write('b576_rvspecfit_results.fits', overwrite=True)
